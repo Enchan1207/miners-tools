dbc10e447503613f24c9e60d3ead414e4767abf7
enchan <enchant0130@gmail.com>
2026-03-21 22:57:20 +0900

    feat: #6 ストラテジを定義, 伐採ストラテジを実装

--- src/main/java/me/enchan/minerstools/MinersToolsMod.java
+++ src/main/java/me/enchan/minerstools/MinersToolsMod.java
@@ -1,13 +1,13 @@
 package me.enchan.minerstools;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import me.enchan.minerstools.bulk_break.HarvestDispatcher;
 import me.enchan.minerstools.payloads.MinersToolsModeTogglePayload;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
@@ -16,7 +16,6 @@ import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
-import net.minecraft.util.math.BlockPos;
 
 public class MinersToolsMod implements ModInitializer {
     public static final String ModId = "miners-tools";
@@ -34,47 +33,10 @@ public class MinersToolsMod implements ModInitializer {
                 MinersToolsModeTogglePayload.CODEC);
 
         PlayerBlockBreakEvents.AFTER.register((world, player, origin, state, blockEntity) -> {
-            Logger.info("broken: %s".formatted(state.getBlock().toString()));
-
-            // 有効でなければ何もしない
             if (!toolStatusByPlayer.getOrDefault(player.getUuid(), false)) {
                 return;
             }
-
-            // 破壊時にメインハンドに持っているアイテムを取得しておく (耐久値考慮などへの利用を想定)
-            var tool = player.getMainHandStack().getItem();
-            MinersToolsMod.Logger.info(tool.toString());
-
-            var queue = new LinkedList<BlockPos>();
-            queue.add(origin);
-
-            int broken = 0;
-
-            while (!queue.isEmpty()) {
-                var pos = queue.poll();
-
-                var candidates = BlockPos
-                        // 三軸方向にクエリして原点だけ除外 原点からの距離4ブロックまで
-                        .stream(pos.add(-1, -1, -1), pos.add(1, 1, 1))
-                        .filter(p -> !p.equals(pos))
-                        .filter(p -> p.isWithinDistance(origin, 4))
-                        // streamで取得されるBlockPosは参照なので、ここで変換
-                        .map(p -> p.toImmutable())
-                        .filter(p -> world.getBlockState(p).getBlock().equals(state.getBlock()))
-                        .toList();
-
-                broken += candidates.size();
-
-                for (var candidate : candidates) {
-                    world.breakBlock(candidate, true, player);
-                }
-
-                queue.addAll(candidates);
-            }
-
-            if (broken > 0) {
-                player.sendMessage(Text.literal("Chain! %d brockes were broken.".formatted(broken + 1)), true);
-            }
+            HarvestDispatcher.onBreakBlock(world, origin, player, state);
         });
 
         ServerPlayNetworking.registerGlobalReceiver(MinersToolsModeTogglePayload.ID, (payload, context) -> {
